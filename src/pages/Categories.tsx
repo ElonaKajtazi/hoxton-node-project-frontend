c90b392216a7ad97e92d38dbd958e07b7cd5fab2
Demir Boraj <demirboraj97@gmail.com>
2022-09-29 16:39:09 +0200

    1

--- src/pages/Categories.tsx
+++ src/pages/Categories.tsx
@@ -1,9 +1,33 @@
+import Button from "@mui/material/Button/Button";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import { Header } from "../components/Header";
+import { Category } from "../types";
+import AutoStoriesIcon from '@mui/icons-material/AutoStories';
 
 export function Categories (){
+    const [categories, setCategories] = useState<Category[]>([]);
+    function getDatafromServer() {
+      fetch("http://localhost:4444/categories/")
+        .then((resp) => resp.json())
+        .then((data) => setCategories(data));
+    }
+  
+    useEffect(() => {
+      getDatafromServer();
+    }, []);
     return (
        <>
  <Header/>
+ <div className="categories">
+    {categories.map((category) => (
+        <Link to={`/categories/${category.id}`}>
+            <div className="category" key={category.id}>
+               <Button size="large" variant="outlined" >{category.name}</Button>
+            </div>
+        </Link>
+    ))}
+ </div>
        </>
     )
 }
