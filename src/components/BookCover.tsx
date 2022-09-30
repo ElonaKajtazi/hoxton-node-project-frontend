5d82e58b30657286f5b390fd56ba9e7674690fd3
Demir Boraj <demirboraj97@gmail.com>
2022-09-30 12:00:03 +0200

    1

--- src/components/BookCover.tsx
+++ src/components/BookCover.tsx
@@ -8,8 +8,10 @@ export function BookCover({ book }: Props) {
   return (
     <Link to={`/books/${book.id}`} key={book.id}>
       <div className="singleBook" key={book.id}>
+        <div className="book">
         <img className="book-img" src={book.image} />
         <h3 className="book-title">{book.title}</h3>
+        </div>
       </div>
     </Link>
   );
